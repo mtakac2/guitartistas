99088b6332fbce27e87cbcac4cf2ec35978e19bb
Marek Takac <takacmarek1221@gmail.com>
2012-07-19 23:32:34 +0200

    Enable email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address  => "smtp.gmail.net",
-  :port  => 25,
-  :user_name  => "guitartistas@gmail.com",
-  :password  => "neustart",
-  :authentication  => :login
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'guitartistas.herokuapp.com',
+  :user_name            => 'guitartistas@gmail.com', # full email address (user@your.host.name.com)
+  :password             => 'password',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true
 }
