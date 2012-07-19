60502268294fa4460419ff205822a19280b5ca94
Marek Takac <takacmarek1221@gmail.com>
2012-07-19 23:21:23 +0200

    Email without tls

--- config/environment.rb
+++ config/environment.rb
@@ -4,21 +4,19 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Guitartistas::Application.initialize!
 
-Guitartistas::Application.configure do
-require 'tlsmail'    
-Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+Guitartistas::Application.configure do  
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.smtp_settings = {
-  :enable_starttls_auto => true,  
-  :address            => 'smtp.gmail.com',
-  :port               => 587,
-  :tls                => true,
-  :domain             => 'gmail.com',
-  :authentication     => :plain,
-  :user_name          => 'guitartistas@gmail.com',
-  :password           => 'neustart'
-}
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,  
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :tls                => true,
+    :domain             => 'gmail.com',
+    :authentication     => :plain,
+    :user_name          => 'guitartistas@gmail.com',
+    :password           => 'neustart'
+  }
 end
