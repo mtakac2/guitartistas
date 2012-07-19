19d9a79a671854a39e1e3b37ef5699f577c753f3
Marek Takac <takacmarek1221@gmail.com>
2012-07-19 23:15:28 +0200

    Email server configurations

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,20 @@ require File.expand_path('../application', __FILE__)
 Guitartistas::Application.initialize!
 
 Guitartistas::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
+require 'tlsmail'    
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,  
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :tls                => true,
+  :domain             => 'gmail.com',
+  :authentication     => :plain,
+  :user_name          => 'guitartistas@gmail.com',
+  :password           => 'neustart'
+}
 end
