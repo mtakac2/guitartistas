5efd2f356461706ee3df674d47047a62ecf36011
Marek Takac <takacmarek1221@gmail.com>
2012-07-19 23:02:07 +0200

    Repair error in enviroment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.raise_delivery_errors = true
-
 # Initialize the rails application
 Guitartistas::Application.initialize!
+
+Guitartistas::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+end
